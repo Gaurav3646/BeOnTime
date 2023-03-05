aa60548921af39100f3608904893c1e1550f50ac
Gaurav <gauravverma3646@gamil.com>
2023-03-06 03:57:20 +0530

    connected with firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import WelcomePage from './Pages/WelcomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
 
 function App() {
   return (
-    <WelcomePage/>
+    
+    <div className="App">
+      {/* <WelcomePage/> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        
+      </Routes>
+
+    </Router>
+    </div>
   );
 }
 
